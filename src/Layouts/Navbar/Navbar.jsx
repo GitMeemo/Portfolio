9276dd15a5aeb29b65d79b621450be0719349460
GitMeemo <ameenkamangar@gmail.com>
2026-01-22 03:01:40 +0500

    Hero-Section BG Added + Random dots effect

--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     return (
-        <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+        <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
             <nav className="container mx-auto px-20 flex items-center justify-between">
                 <a href="#" className="text-xl font-bold tracking-tight hover:text-primary" >
                     AK<span className="text-primary">.</span>
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <div className="hidden md:flex items-center gap-1">
                     <div className="glass rounded-full px-2 flex items-center gap-1">
                         {navLinks.map((link, index) => (
-                            <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">
+                            <a href={link.href} key={index} className="px-4 py-2 text-sm text-primary-foregroundforeground hover:text-primary rounded-full hover:bg-background">
                                 {link.label}</a>))}
                     </div>
                     {/* <div  >
