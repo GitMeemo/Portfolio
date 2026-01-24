9912e0482cf1cda3600e49dee20a1f7687126377
GitMeemo <ameenkamangar@gmail.com>
2026-01-24 23:46:34 +0500

    Project Grids added

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -36,12 +36,29 @@ const Projects = () => {
                 <span className='text-white text-sm font-medium tracking-wider uppercase animate-fade-in'>
                     Featured Work
                 </span>
-                <h2 className='text-4xl md:text-5xl font-bold mt-4 mb-6 animation-fade-in animation-delay-100 text-secondary-foreground glow-text'>Projects that
+                <h2 className='text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground glow-text'>Projects that
                     <span> make an impact.</span>
                 </h2>
-                <p>
-                    
+                <p className="text-muted-foreground animate-fade-in animation-delay-200">
+                    Highlights of my recent work, including scalable web applications.
                 </p>
+            </div>
+            {/* Projects Grid */}
+            <div className='grid md:grid-cols-2 gap-8'>
+                {projects.map((project, id) => (
+                    <div key={id}
+                        className="group glass rounded-2xl overflow-hidden animate-fade-in md:row-span-1"
+                        style={{ animationDelay: `${(id + 1) * 100}ms` }}
+                    >
+                        {/* image */}
+                        <div className='relative overflow-hidden aspect-video'>
+                            <img src={project.image} alt={project.title} className="w=full h=full object-cover transition-transform duration-700 group-hover:scale-110" />
+                            <div
+                                className="absolute inset-0 bg-linear-to-t from-card via-card/50 to-transparent opacity-60"
+                            />
+                        </div>
+                    </div>
+                ))}
             </div>
 
         </div>
