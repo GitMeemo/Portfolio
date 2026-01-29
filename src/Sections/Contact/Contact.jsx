95dc28be220024af50abc16f42c4b4c5607ec3a6
GitMeemo <ameenkamangar@gmail.com>
2026-01-29 19:33:03 +0500

    Contact Section Started

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,3 +1,24 @@
+import { Mail, Phone, MapPin, CheckCircle, AlertCircle } from "lucide-react";
+const contactInfo = [
+    {
+        icon: Mail,
+        label: "Email",
+        value: "ameenkamangar@gmail.com",
+        href: "mailto:ameenkamangar@gmail.com"
+    },
+    {
+        icon: Phone,
+        label: "Phone",
+        value: "+92 3097482142",
+        href: "tel:+923097482142"
+    },
+    {
+        icon: MapPin,
+        label: "Location",
+        value: "Sukkur, Pakistan",
+        href: ""
+    },
+]
 const Contact = () => {
     return <section></section>
 }
