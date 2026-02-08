3535af453a7079cff9d0612447ed768762ada4d2
GitMeemo <ameenkamangar@gmail.com>
2026-02-08 14:56:14 +0500

    Email issue resolved

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
         try {
             const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
             const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-            const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+            const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY_ID;
 
             if (!serviceId || !templateId || !publicKey) {
                 throw new Error(
